1582ac73f64e548c778cad7afbf494d4baeb7c44
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2021-07-16 20:07:37 +0530

    Double-Checked Locking condition on Singleton,committed and Pushed.

--- design-principles-patterns/src/com/patterns/singleton/DateUtil.java
+++ design-principles-patterns/src/com/patterns/singleton/DateUtil.java
@@ -1,16 +1,27 @@
 package com.patterns.singleton;
 
 public class DateUtil {
+    /**
+     * For double-checked Locking the field is declared volatile;
+     * to get rid of cache coherence issue.
+     * 
+     * As Java Memory Model could allow the publication of 
+     *  partially initialized objects,so the filed should be
+     *  volatile. 
+     */
+	private static volatile DateUtil dateUtilInstance;
 
-	private static DateUtil dateUtilInstance;
-	
 	private DateUtil() {
-		
+
 	}
+
 	public static DateUtil getDateUtilInstance() {
-		if(dateUtilInstance == null)
-			dateUtilInstance = new DateUtil();
+		if (dateUtilInstance == null) {
+			synchronized (DateUtil.class) {
+				if (dateUtilInstance == null)
+					dateUtilInstance = new DateUtil();
+			}
+		}
 		return dateUtilInstance;
-		
 	}
 }
